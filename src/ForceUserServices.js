028666056ab10479487c6a9bf44a51aa90d31fe5
Abraham <abdiaz2018@gmail.com>
2021-08-13 16:03:38 -0700

    added fetch method

--- src/ForceUserServices.js
+++ src/ForceUserServices.js
@@ -2,4 +2,16 @@ class ForceUserServices {
     constructor(baseURL){
         this.baseURL = baseURL;
     };
+
+    getForceUsers(){
+        fetch(this.baseURL)
+        .then(resp => resp.json())
+        .then(forceusers => {
+            console.log(forceusers)
+            forceusers.forEach(user => {
+                const newUser = new ForceUser(user)
+                newUser.renderForceUser();
+            })
+        })
+    };
 }
