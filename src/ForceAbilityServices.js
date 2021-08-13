a61252f4ac3c3a0212d9a82acb5e63a08ed1bf25
Abraham <abdiaz2018@gmail.com>
2021-08-13 16:06:50 -0700

    force ability services class finished

--- src/ForceAbilityServices.js
+++ src/ForceAbilityServices.js
@@ -1,1 +1,11 @@
+class ForceAbilityServices {
+    constructor(baseURL){
+        this.baseURL = baseURL;
+    };
+
+    getForceAbilities(){
+        fetch(this.baseURL)
+        .then(resp => resp.json())
+        .then(forceabilities => console.log(forceabilities))
+    };
 }
