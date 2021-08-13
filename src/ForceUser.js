497e6968f6dfb899f7c693901770044496ff3f1d
Abraham <abdiaz2018@gmail.com>
2021-08-13 15:56:59 -0700

    force user class created

--- src/ForceUser.js
+++ src/ForceUser.js
@@ -1,1 +1,10 @@
+class ForceUser {
+    constructor({id, name, team, gender, race, force_ability}){
+        this.id = id;
+        this.name = name;
+        this.team = team;
+        this.gender = gender; 
+        this.race = race
+        this.force_ability = force_ability
+    };
 };
