8480150da8364b67638b828b5ca4417e594d2760
Abraham <abdiaz2018@gmail.com>
2021-08-20 16:48:29 -0700

    functioning create method

--- src/ForceUserServices.js
+++ src/ForceUserServices.js
@@ -15,6 +15,38 @@ class ForceUserServices {
         })
     };
 
+    createUser() {
+        const userForm = document.getElementById('force-user-form')
+        const nameInput = document.getElementById('name')
+        const teamInput = document.getElementById('team')
+        const genderInput = document.getElementById('gender')
+        const raceInput = document.getElementsById('race')
+        const ability = document.getElementById('ability-select')
+
+        const formData = {
+            name: nameInput.value,
+            team: teamInput.value,
+            gender: genderInput.value,
+            race: raceInput.value,
+            ability_id: ability.value 
+        }
+        const config = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+        };
+
+        fetch(baseURL, config)
+        .then(resp => resp.json())
+        .then(user => {
+            renderForceUser(user);
+            userForm.requestFullscreen();
+        });
+    }
+
     deleteUser(id) {
         const config = {
             method: 'DELETE'
