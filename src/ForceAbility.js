dbc1d3b6df55914d3dc62eec079bc960096b61da
Abraham <abdiaz2018@gmail.com>
2021-08-20 15:56:00 -0700

    populating ability dropdown

--- src/ForceAbility.js
+++ src/ForceAbility.js
@@ -6,7 +6,8 @@ class ForceAbility {
     createAbilityOption() {
         const collection = document.getElementById('ability-select');
         const abiltyOption = document.createElement('option');
-        abiltyOption.innerHTML = `${this.name}`
+        abiltyOption.value = this.id
+        abiltyOption.innerHTML = this.name
         collection.appendChild(abiltyOption);
     };
     
