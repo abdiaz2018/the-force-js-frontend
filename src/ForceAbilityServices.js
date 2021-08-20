3413593b28702922aca01e4285b8461c63b2dff1
Abraham <abdiaz2018@gmail.com>
2021-08-20 15:44:25 -0700

    option method iterated

--- src/ForceAbilityServices.js
+++ src/ForceAbilityServices.js
@@ -7,7 +7,10 @@ class ForceAbilityServices {
         fetch(this.baseURL)
         .then(resp => resp.json())
         .then(forceabilities => {
-            console.log(forceabilities)
+            forceabilities.forEach(ability => {
+                const newAbility = new ForceAbility(ability)
+                newAbility.createAbilityOption();
+            })
         })
     };
 }
